553b1cad6b0e38fbe2e24088b5215bcec9bce07e
MRXbef <adambayu878@gmail.com>
2024-08-25 04:24:22 +0800

    vercel rebuild

--- server/routers/index.js
+++ server/routers/index.js
@@ -7,7 +7,7 @@ const router = express.Router()
 router.post('/register', register)
 router.post('/login', login)
 router.delete('/logout', logout)
-router.get('/users', verifyToken, getUsers)
+router.get('/users', getUsers)
 
 router.get('/', testGet)
 
