c57214da5006c4c8b3573bdec0e366184884c7a0
MRXbef <adambayu878@gmail.com>
2024-08-25 02:24:26 +0800

    vercel rebuild

--- server/config/Database.js
+++ server/config/Database.js
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize"
+import sqlite3 from "sqlite3"
 
 const db = new Sequelize({
     dialect: 'sqlite',
-    storage: 'mydatabase.db'
+    storage: 'mydatabase.db',
+    dialectModule: sqlite3
 })
 
 export default db
