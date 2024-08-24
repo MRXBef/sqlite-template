f532b0e430e0a5e34c2d90c34911d99271947c9b
MRXbef <adambayu878@gmail.com>
2024-08-25 04:54:08 +0800

    vercel rebuild

--- server/config/Database.js
+++ server/config/Database.js
@@ -1,15 +1,16 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { Sequelize } from 'sequelize';
-import path from 'path';
 import sqlite3 from 'sqlite3';
 
+// Mendapatkan __dirname dalam modul ES6
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Menetapkan path database ke folder /tmp
 const db = new Sequelize({
     dialect: 'sqlite',
-    storage: path.resolve(__dirname, 'mydatabase.db'),
+    storage: './tmp/mydatabase.db', // Gunakan folder /tmp untuk penyimpanan sementara
     logging: console.log,
     dialectModule: sqlite3
 });
