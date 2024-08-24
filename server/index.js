2b6f58a7b90e37d1f29722c31ba4100e33d424a0
MRXbef <adambayu878@gmail.com>
2024-08-25 03:36:18 +0800

    vercel rebuild

--- server/index.js
+++ server/index.js
@@ -1,36 +1,28 @@
-import express, { json } from 'express';
-import dotenv from 'dotenv';
-import db from './config/Database.js';
-import Users from './models/userModel.js';
-import router from './routers/index.js';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express, {json} from 'express'
+import dotenv from 'dotenv'
+import db from './config/Database.js'
+import Users from './models/userModel.js'
+import router from './routers/index.js'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const PORT = process.env.PORT;
+const app = express()
+const PORT = process.env.PORT
 
-const startServer = async () => {
-    try {
-        await db.authenticate();
-        console.log('Database connected');
-        await db.sync(Users); // pastikan `sync` tidak memerlukan argumen jika Anda tidak menggunakannya
+try {
+    await db.authenticate()
+    console.log('database connected')
 
-        // Setup middleware
-        app.use(cors());
-        app.use(cookieParser());
-        app.use(json());
-        app.use(router);
+    await db.sync(Users)
+} catch (error) {
+    console.log('error: ' + error)
+}
 
-        // Start server
-        app.listen(PORT, () => {
-            console.log(`App listening at port ${PORT}`);
-        });
-    } catch (error) {
-        console.log('Error: ' + error);
-        process.exit(1); // Optional: exit the process with an error code
-    }
-};
+app.use(cors())
+app.use(cookieParser())
+app.use(json())
+app.use(router)
 
-startServer();
+app.listen(PORT, () => {console.log(`app listen at port ${PORT}`)})
