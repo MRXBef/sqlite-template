10352bf73be5bda686c3c396271dce6fc22e5460
MRXbef <adambayu878@gmail.com>
2024-08-25 21:37:27 +0800

    add pg pg-hstore into a db

--- server/config/Database.js
+++ server/config/Database.js
@@ -1,26 +1,9 @@
-// import { fileURLToPath } from 'url';
-// import { dirname } from 'path';
-// import { Sequelize } from 'sequelize';
-// import path from 'path';
-// import sqlite3 from 'sqlite3'
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const db = new Sequelize({
-//     dialect: 'sqlite',
-//     storage: path.resolve(__dirname, 'mydatabase.db'),
-//     logging: console.log,
-//     dialectModule: sqlite3
-// });
-
-// export default db;
-
 import { Sequelize } from "sequelize"
+import dotenv from 'dotenv'
 
-const connectionString = "postgres://default:2YK8HEohgJyU@ep-lingering-sun-a1czpp4t.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
+dotenv.config()
 
-const db = new Sequelize(connectionString, {
+const db = new Sequelize(process.env.PSQL, {
     dialect: "postgres",
     protocol: "postgres",
     dialectOptions: {
